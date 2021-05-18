5f89cc57370097212896c6e0d24cd71e5eefbe37
leida <zhang20084@126.com>
2021-05-18 16:02:38 +0800

    fix:优化显示的演示json数据

--- example/App.vue
+++ example/App.vue
@@ -1,110 +1,135 @@
 <template>
-  <div>
+  <div class="content">
+    <h1 class="title">easyapi-json-form</h1>
     <easyapi-json-form :jsonData="jsonData" ref="jsonForm"></easyapi-json-form>
-    <el-button @click="getJSONFormData">getJSONFormData 获取 JSON 表单数据</el-button>
-    <el-button @click="exportJSON">exportJSON 导出标准 JSON 数据</el-button>
+    <div class="tools-btn pure-form">
+      <el-button class="pure-button" @click="getJSONFormData">getJSONFormData</el-button>
+      <el-button class="pure-button" @click="exportJSON">exportJSON</el-button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "app",
-  data: function() {
-    return {
-      jsonData: [
-        {
-          id: 1,
-          name: "blog",
-          type: "object",
-          description: "博客",
-          required: false,
-          sample: "",
-          demo: "",
-          childs: [
-            {
-              id: 10,
-              name: "title",
-              type: "array",
-              description: "标题",
-              required: true,
-              sample: "我和我的祖国",
-              demo: "",
-              childs: [
-                {
-                  id: 100,
-                  name: "title",
-                  type: "object",
-                  description: "标题",
-                  required: true,
-                  sample: "我和我的祖国",
-                  demo: "",
-                  childs: [
-                    {
-                      id: 1000,
-                      name: "title",
-                      type: "string",
-                      description: "标题",
-                      required: true,
-                      sample: "我和我的祖国",
-                      demo: "",
-                      childs: []
-                    }
-                  ]
-                },
-                {
-                  id: 101,
-                  name: "title",
-                  type: "string",
-                  description: "标题",
-                  required: true,
-                  sample: "我和我的祖国",
-                  demo: "",
-                  childs: []
-                }
-              ]
-            }
-          ]
-        },
-        {
-          id: 2,
-          name: "title",
-          type: "array",
-          description: "标题",
-          required: true,
-          sample: "我和我的祖国",
-          demo: "",
-          childs: [
-            {
-              id: 20,
-              name: "title",
-              type: "string",
-              description: "标题",
-              required: true,
-              sample: "我和我的祖国",
-              demo: "",
-              childs: []
-            }
-          ]
-        }
-      ]
-    };
-  },
-  methods: {
-    getJSONFormData() {
-      let d = this.$refs.jsonForm.getJSONFormData();
-      console.log(d);
+  export default {
+    name: "app",
+    data: function () {
+      return {
+        jsonData: [
+          {
+            id: 1,
+            name: "blog",
+            type: "object",
+            description: "博客",
+            required: false,
+            sample: "",
+            demo: "",
+            childs: [
+              {
+                id: 10,
+                name: "user",
+                type: "array",
+                description: "作者",
+                required: true,
+                sample: "我和我的祖国",
+                demo: "",
+                childs: [
+                  {
+                    id: 100,
+                    name: "name",
+                    type: "object",
+                    description: "名称",
+                    required: true,
+                    sample: "张三",
+                    demo: "",
+                    childs: []
+                  }, {
+                    id: 101,
+                    name: "email",
+                    type: "string",
+                    description: "邮箱",
+                    required: true,
+                    sample: "zhangsan@qq.com",
+                    demo: "",
+                    childs: []
+                  }, {
+                    id: 102,
+                    name: "company",
+                    type: "object",
+                    description: "公司",
+                    required: true,
+                    sample: "接口有限公司",
+                    demo: "",
+                    childs: [
+                      {
+                        id: 1021,
+                        name: "address",
+                        type: "string",
+                        description: "公司地址",
+                        required: true,
+                        sample: "中国无锡",
+                        demo: "",
+                        childs: []
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            id: 2,
+            name: "comments",
+            type: "array",
+            description: "评论",
+            required: true,
+            sample: "我和我的祖国",
+            demo: "",
+            childs: [
+              {
+                id: 20,
+                name: "title",
+                type: "string",
+                description: "评论标题",
+                required: true,
+                sample: "写的不错",
+                demo: "写的不错",
+                childs: []
+              }, {
+                id: 21,
+                name: "content",
+                type: "string",
+                description: "评论内容",
+                required: true,
+                sample: "最是我见过最棒的文章",
+                demo: "",
+                childs: []
+              }
+            ]
+          }
+        ]
+      };
     },
-    exportJSON() {
-      let d = this.$refs.jsonForm.exportJSON();
-      console.log(d);
+    methods: {
+      getJSONFormData() {
+        let d = this.$refs.jsonForm.getJSONFormData();
+        console.log(d);
+      },
+      exportJSON() {
+        let d = this.$refs.jsonForm.exportJSON();
+        console.log(d);
+      }
+    },
+    mounted: function () {
     }
-  },
-  mounted: function() {}
-};
+  };
 </script>
 
 <style>
-@import url("../node_modules/highlight.js/styles/github.css");
+  @import url("../node_modules/highlight.js/styles/github.css");
+
+  .tools-btn {
+    margin-top: 20px;
+  }
 </style>
 
 
--- src/index.js
+++ src/index.js
@@ -39,4 +39,4 @@ export default install
 export const components = {
   JsonForm,
   VERSION
-}
+};
